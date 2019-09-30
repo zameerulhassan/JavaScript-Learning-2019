83d0b4e3ec170e77421701867676355f30806f59
Zam Hassan <zameerulhassan@gmail.com>
2019-09-29 22:43:07 -0600

    initial commit-29 Sept-2019

---
+++ app.js
@@ -0,0 +1,5 @@
+var variableInJS = "Hello World"; // this is global variable
+function myFunction() // this global function because this is not inside a function.
+{
+
+}
---
+++ program1.js
@@ -0,0 +1,3 @@
+document.write("Hello World3");
+document.write("<h1>Hello World3</h1>");
+document.write("<p>Hello World3</p1>");
